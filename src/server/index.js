bdd2e38dddb76e7a70003db56e3a0d993aae8f4b
Jeffrey Hwang <jehwang@riotgames.com>
2018-09-13 16:10:16 -0700

    Enabled CORS for mock express service

--- src/server/index.js
+++ src/server/index.js
@@ -3,8 +3,23 @@ const os = require('os');
 const bodyParser = require('body-parser');
 
 const app = express();
+
+// Parse body as JSON
 app.use(bodyParser.json())
+
+// Specify static directory
 app.use(express.static('dist'));
+
+// CORS
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
+/**************************************
+ * Routes
+ *************************************/
 
 app.get('/api/getUsername', (req, res) => {
   return res.send({ username: os.userInfo().username });
@@ -13,13 +28,13 @@ app.get('/api/getUsername', (req, res) => {
 // Mock Sensor
 app.get('/sensor', (req, res) => {
   return res.send({
-    heading: 12,
+    heading: 30,
     distance: 199,
   });
 });
 
 // Mock Move
-app.post('/move', (req, res) => {
+app.post('/vectormove', (req, res) => {
   console.log(req.body);
   return res.json({
     status: "ok"

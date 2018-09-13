8a1190e29423139d85cbb1279bff38ba841fa8d3
Jeffrey Hwang <jehwang@riotgames.com>
2018-09-13 15:14:35 -0700

    Added mock route for move

--- src/server/index.js
+++ src/server/index.js
@@ -4,5 +4,24 @@ const os = require('os');
 const app = express();
 
 app.use(express.static('dist'));
-app.get('/api/getUsername', (req, res) => res.send({ username: os.userInfo().username }));
+
+app.get('/api/getUsername', (req, res) => {
+  return res.send({ username: os.userInfo().username });
+});
+
+// Mock Sensor
+app.get('/sensor', (req, res) => {
+  return res.send({
+    heading: 12,
+    distance: 199,
+  });
+});
+
+// Mock Move
+app.post('/move', (req, res) => {
+  return res.json({
+    status: "ok"
+  });
+});
+
 app.listen(8080, () => console.log('Listening on port 8080!'));
